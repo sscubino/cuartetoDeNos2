b67af7d5b4839ef5d4564a15eafd80075047d18e
mdemare <mdemare@debmedia.com>
2020-06-27 12:55:32 -0300

    Agrego tests de silencios

--- tests/silencios.cpp
+++ tests/silencios.cpp
@@ -16,3 +16,22 @@ TEST(silenciosTEST, silenciosValidos){
     ASSERT_INTERVALOS_EQ(silencios(hablante, prof, freq, umbral), intervalos);
 }
 
+TEST(silenciosTEST, sinSilencios){
+    senial hablante = {1, 5, -3, 8, 6, 0, -5, -8, 9, -1};
+    int prof = 8;
+    int freq = 10;
+    int umbral = 5;
+
+    vector<intervalo> intervalos = {};
+    ASSERT_INTERVALOS_EQ(silencios(hablante, prof, freq, umbral), intervalos);
+}
+
+TEST(silenciosTEST, todoSilencio){
+    senial hablante = {1, 5, -3, 8, 6, 0, -5, -8, 9, -1};
+    int prof = 8;
+    int freq = 10;
+    int umbral = 10;
+
+    vector<intervalo> intervalos = {make_pair(0, hablante.size()-1)};
+    ASSERT_INTERVALOS_EQ(silencios(hablante, prof, freq, umbral), intervalos);
+}
