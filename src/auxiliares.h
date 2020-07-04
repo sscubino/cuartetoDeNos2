e26440578cf1cfb0b71c29f8888f5d55ff38ff85
holasoyivo <ivangimenez8727@gmail.com>
2020-07-04 17:57:06 -0300

    Agregué el nuevo burbujeo al .h

--- src/auxiliares.h
+++ src/auxiliares.h
@@ -11,6 +11,8 @@ bool enRango(int muestra, int prof);
 float tono(senial s);
 float tonoRango(senial s, int desde, int hasta);
 float duracion (intervalo interv, int freq);
+void burbujeoReuniones (reunion &r, int i);
+void swapReuniones (reunion &r, int i, int j);
 void ASSERT_SENIAL_EQ(senial s1, senial s2);
 void ASSERT_REUNION_EQ(reunion reunion1, reunion reunion2);
 void ASSERT_HABLANTES_EQ(vector<hablante> s1, vector<hablante> s2);
